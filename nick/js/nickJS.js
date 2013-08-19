ebd909a3f9821712f0740007ad7b49673432a119
Mitch Colleran <mitchcolleran@gmail.com>
2013-08-18 20:58:41 -0700

    poor attempt at jquery

--- nick/js/nickJS.js
+++ nick/js/nickJS.js
@@ -1,8 +1,19 @@
 $(document).ready(
 function() {
-    $('#fullPage').css({
-            width: $(document).width(),
-            height: $(document).height()
+    $('.fullPage').css({
+            width: $(window).width(),
+            height: $(window).height()
         });
 }
 );
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
+function printTest() {
+	console.log("test test")
+};
